4b33da0f03458bc656f7f542502211d19ea446c2
tomoka1001 <tomo.101.2382@gmail.com>
2026-04-06 10:58:16 +0900

    java学習の進捗報告「9クラス型,コントラクタ」_yamamoto

---
+++ bouken/Main.java
@@ -0,0 +1,23 @@
+package bouken;
+
+public class Main {
+    public static void main(String[] args) {
+        Hero h = new Hero();
+        h.name = "ミナト";
+        h.hp = 100;
+
+        Matango m1 = new Matango();
+        m1.hp = 50;
+        m1.suffix = 'A';
+
+        Matango m2 = new Matango();
+        m2.hp = 50;
+        m2.suffix = 'B';
+
+        h.slip();
+        m1.run();
+        m2.run();
+        h.run();
+    }
+    
+}
---
+++ japp/comment/Zenhan.java
@@ -0,0 +1,12 @@
+package japp.comment;
+
+public class Zenhan {
+    public static void doWarusa() {
+        System.out.println("きなこでござる。食えませんがの。");
+    }
+
+    public static void doTogame() {
+        System.out.println("この老ぼれの目はごまかせませんぞ。");
+    }
+    
+}
